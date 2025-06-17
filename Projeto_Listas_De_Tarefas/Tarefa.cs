5268ae87c8a78939ac23df38f33cd7a53c825199
joao Berenguer <joaovictorberenguer@gmail.com>
2025-06-17 17:49:14 -0300

    Adicionando a classe tarefa com propriedades para armazenar tarefas

---
+++ Projeto_Listas_De_Tarefas/Tarefa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Listas_De_Tarefas
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public bool Concluida { get; set; }
+        public Tarefa (int id, string descricao)
+        {
+            Id = id;
+            Descricao = descricao;
+            Concluida = false;
+        }
+    }
+}
